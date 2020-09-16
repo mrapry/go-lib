e275ef4fb815f7ebcd1472298578659245c800b5
Matius Apry Prastowo <mr.apry.prastowo@gmail.com>
2020-09-16 11:01:51 +0700

    fixing sdk not used in sdk

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -2,8 +2,6 @@ package sdk
 
 import (
 	auth "github.com/mrapry/go-lib/sdk/auth-service"
-	master_service "github.com/mrapry/go-lib/sdk/master-service"
-	notification_service "github.com/mrapry/go-lib/sdk/notification-service"
 )
 
 // Option func type
@@ -16,25 +14,9 @@ func SetAuthService(authService auth.ServiceAuth) Option {
 	}
 }
 
-// SetMasterService option func
-func SetMasterService(masterService master_service.MasterService) Option {
-	return func(s *sdkInstance) {
-		s.masterService = masterService
-	}
-}
-
-// SetNotificationService option func
-func SetNotificationService(notificationService notification_service.ServiceNotification) Option {
-	return func(s *sdkInstance) {
-		s.notificationService = notificationService
-	}
-}
-
 // SDK instance abstraction
 type SDK interface {
 	AuthService() auth.ServiceAuth
-	MasterService() master_service.MasterService
-	NotificationService() notification_service.ServiceNotification
 }
 
 // NewSDK constructor with each service option
@@ -49,19 +31,9 @@ func NewSDK(opts ...Option) SDK {
 }
 
 type sdkInstance struct {
-	authService         auth.ServiceAuth
-	masterService       master_service.MasterService
-	notificationService notification_service.ServiceNotification
+	authService auth.ServiceAuth
 }
 
 func (s *sdkInstance) AuthService() auth.ServiceAuth {
 	return s.authService
-}
-
-func (s *sdkInstance) MasterService() master_service.MasterService {
-	return s.masterService
-}
-
-func (s *sdkInstance) NotificationService() notification_service.ServiceNotification {
-	return s.notificationService
 }
